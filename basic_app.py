57527b0e145ffe4561dda6fe862e517000c2895d
Chris <cgarciaguy@gmail.com>
2022-02-01 22:42:22 -0600

    trying with spotipy.Spotify(auth=token)

--- basic_app.py
+++ basic_app.py
@@ -107,8 +107,8 @@ def home():
 @app.route('/user_data')
 def user_data():
 	
-	auth_manager.get_access_token(session.get('access_token'))
-	sp = spotipy.Spotify(auth_manager=auth_manager)
+	# auth_manager.get_access_token(session.get('access_token'))
+	sp = spotipy.Spotify(auth=session.get('access_token'))
 
 	if not request.args.get('time_range'):
 		return redirect('/user_data?time_range=short_term&search=tracks')
